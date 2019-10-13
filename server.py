d69fbb7f39c53b346a7a3cc48b6148528dc3ee44
blackle <isabelle@blackle-mori.com>
2019-10-13 17:44:50 -0400

    lots of random stuff

--- server.py
+++ server.py
@@ -1,9 +1,58 @@
 #!/usr/bin/env python3
 
 from flask import Flask, escape, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 from email.header import decode_header
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Challenge(db.Model):
+	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+	prefix = db.Column(db.Text)
+	nonce_length = db.Column(db.Integer)
+
+	def __init__(self, prefix, nonce_length, id = None):
+		self.id = id
+		self.prefix = prefix
+		self.nonce_length = nonce_length
+
+class ChallengeSchema(ma.Schema):
+	class Meta:
+		fields = ("id", "prefix", "nonce_length")
+
+challengeSchema = ChallengeSchema()
+challengesSchema = ChallengeSchema(many = True)
+
+@app.route('/challenge', methods = ['GET'])
+def get_challenges():
+	challenges = Challenge.query.all()
+	result = challengesSchema.dump(challenges)
+
+	return jsonify(result)
+
+#class Solution(db.Model):
+#	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#	hash = db.Column(db.String(64))
+#	nonce = db.Column(db.String(64))
+#	session_id = db.Column(db.Integer)
+#
+#	def __init__(self, hash, nonce, session_id):
+#		self.hash = hash
+#		self.nonce = nonce
+#		self.session_id = session_id
+#
+#class SolutionSchema(ma.Schema):
+#	class Meta:
+#		feilds = ('hash', 'nonce')
+#
+#solution_schema = SolutionSchema(strict=True)
 
 def qdecode(string):
 	try:
@@ -11,6 +60,8 @@ def qdecode(string):
 		return decoding[0].decode(decoding[1])
 	except:
 		return ""
+
+
 
 CHALLENGES = [{
 	"id": "0000000000",
